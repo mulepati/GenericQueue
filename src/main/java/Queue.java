90f5f06300b3b7f9b00334d8f1c2ca44e5a1a9ec
samek <sabalmulepati@gmail.com>
2019-02-21 13:47:01 -0800

    test

---
+++ src/main/java/Queue.java
@@ -0,0 +1,108 @@
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+
+public class Queue<E> {
+    private int top = -1;
+    private E[] elements = (E[]) new Object[10];
+
+
+
+    public void add(E value) {
+        if(top >= elements.length - 1) {
+            expandSize();
+        }
+        elements[++top] = value;
+    }
+
+    public E peek() {
+        if(top < 0) {
+            throw new NoSuchElementException();
+        }
+
+        return elements[0];
+    }
+
+    public E remove() {
+        if(top < 0) {
+            throw new NoSuchElementException();
+        }
+        E store = elements[0];
+        remove(0);
+        return store;
+
+    }
+
+    public void removeDuplicates() {
+        if(top < 0) {
+            throw new NoSuchElementException();
+        }
+        for (int i = 0; i < top; i++) {
+            E store = elements[i];
+            for (int j = i; j < top; j++) {
+                if(elements[j].equals(store)){
+                    remove(j);
+                }
+            }
+        }
+    }
+
+    public void removeAll(E value) {
+        if(top < 0) {
+            throw new NoSuchElementException();
+        }
+        int numSwaps = 0;
+        for (int y = 0; y < top; y++) {
+            if(elements[y - numSwaps].equals(value)) {
+                remove(y - numSwaps);
+                numSwaps++;
+            }
+        }
+
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean equals(Object obj) {
+        if (obj instanceof Queue<?> && ((Queue<?>) obj).size() == this.size()) {
+            for (int i = 0; i <= top; i++) {
+                if (!((Queue<?>) obj).get(i).equals(this.get(i))) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+
+    }
+
+    public E get(int index) {
+        if (top < index) {
+            throw new NoSuchElementException();
+        }
+
+        return elements[index];
+    }
+
+    public int size() {
+        return top + 1;
+    }
+    private void expandSize() {
+        int increasedSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, increasedSize);
+    }
+
+    private void remove(int index) {
+        if(top < 0) {
+            throw new NoSuchElementException();
+        }
+        for (int i = index; i < elements.length - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        top--;
+    }
+}
